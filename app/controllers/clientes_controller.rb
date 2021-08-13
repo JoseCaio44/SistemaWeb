7f2e753128358ff72f658416db44dcdd3c18e9a9
Gabrielxdf <gabrielxdf16@gmail.com>
2021-08-13 17:33:49 -0300

    problema do select endereço para os clientes resolvido

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -5,6 +5,7 @@ class ClientesController < ApplicationController
   # GET /clientes or /clientes.json
   def index
     @clientes = Cliente.all
+    @enderecos = Endereco.all
   end
 
   # GET /clientes/1 or /clientes/1.json
